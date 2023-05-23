098ad51f289451c51534cf2be9132bde1a7f8185
hakhant21 <hakhant21@gmail.com>
2023-05-23 13:09:11 +0630

    layout ui guest

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,34 +1,15 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3";
-
-defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    }
-});
+import { Head } from "@inertiajs/vue3";
+import GuestLayout from '@/Layouts/GuestLayout.vue'
 </script>
 
 <template>
     <Head title="Welcome Page" />
-    <div class="bg-white shadow">
-        <div class="max-w-6xl mx-auto py-6 px-8">
-            <div class="flex justify-between items-center">
-                <Link :href="route('welcome')">
-                    <div class="py-1 px-4 rounded-lg shadow-lg bg-slate-700 text-slate-50 hover:bg-slate-600 hover:text-slate-100">Welcome</div>
-                </Link>
-                <div class="flex justify-between items-center">
-                    <Link :href="route('login')">
-                        <div class="py-1 px-4 rounded-lg shadow-lg bg-slate-700 text-slate-50 hover:bg-slate-600 hover:text-slate-100 mr-2">Login</div>
-                    </Link>
-                    <Link :href="route('register')">
-                        <div class="py-1 px-4 rounded-lg shadow-lg bg-slate-700 text-slate-50 hover:bg-slate-600 hover:text-slate-100">Register</div>
-                    </Link>
-                </div>
-            </div>
+
+    <GuestLayout>
+        <div>
+            Welcome
         </div>
-    </div>
+    </GuestLayout>
 
 </template>
