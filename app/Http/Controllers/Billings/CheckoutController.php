066d8237ff2579e67f25c092b7e22713825c6320
hakhant21 <hakhant21@gmail.com>
2023-07-29 14:03:10 +0630

    update

--- app/Http/Controllers/Billings/CheckoutController.php
+++ app/Http/Controllers/Billings/CheckoutController.php
@@ -19,8 +19,6 @@ class CheckoutController extends Controller
             'email' => 'htet@gmail.com'
         ]);
 
-        $session = request()->session()->get('hak_session');
-
-        return redirect()->away($data['webPaymentUrl'])->with('session', $session);
+        return redirect()->away($data['webPaymentUrl']);
     }
 }
